6862db9219633851d0ce9eca82c47543eea2e39e
oskari.kosonen <o.kosonen@gmail.com>
2019-12-20 14:30:51 +0200

    Initial commit of AuthController

---
+++ Kittens.API/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using Kittens.API.Data;
+using Kittens.API.DTOs;
+using Kittens.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kittens.API.Controllers
+{
+    [Route("api/[controller]")]
+	[ApiController]
+    public class AuthController : ControllerBase
+    {
+		private readonly IAuthRepository _repo;
+
+		public AuthController(IAuthRepository repo) {
+			this._repo = repo;
+		}
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            // TOOD: validate
+
+            var username = userForRegisterDto.Username.ToLower();
+            if (await _repo.UserExists(username)) {
+                return BadRequest("Username already exists");
+            }
+
+            var userToCreate = new User {
+                Username = username
+            };
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            // TODO: Return user
+            return StatusCode(201);
+        }
+    }
+}
---
+++ Kittens.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,8 @@
+namespace Kittens.API.DTOs
+{
+    public class UserForRegisterDto
+    {
+        public string Username {get; set;}
+        public string Password {get; set;}
+    }
+}
