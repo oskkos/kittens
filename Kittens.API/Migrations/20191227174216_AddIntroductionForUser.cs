82b02a7514b56c127d5d3c2e9694201563d75634
oskari.kosonen <o.kosonen@gmail.com>
2019-12-27 19:43:55 +0200

    Add missing Introduction column

---
+++ Kittens.API/Migrations/20191227174216_AddIntroductionForUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kittens.API.Migrations
+{
+    public partial class AddIntroductionForUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Introduction",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Introduction",
+                table: "Users");
+        }
+    }
+}
