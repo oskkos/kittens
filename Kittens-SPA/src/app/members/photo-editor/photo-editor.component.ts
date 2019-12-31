61b2e0e23cfaa6d79c71739f1bfdaaf6659390df
oskari.kosonen <o.kosonen@gmail.com>
2019-12-31 13:55:55 +0200

    Photo upload UI

--- Kittens-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ Kittens-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
 import { IPhoto } from '../../api-interfaces';
+import { environment } from 'src/environments/environment';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -9,9 +12,43 @@ import { IPhoto } from '../../api-interfaces';
 export class PhotoEditorComponent implements OnInit {
   @Input() public photos: IPhoto[];
 
-  constructor() { }
+  public uploader: FileUploader;
+  public hasBaseDropZoneOver = false;
+
+  constructor(private authService: AuthService) { }
 
   public ngOnInit() {
+    this.initializeUploader();
   }
 
+  public fileOverBase(e: unknown): void {
+    this.hasBaseDropZoneOver = !!e;
+  }
+
+  private initializeUploader() {
+    this.uploader = new FileUploader({
+      url: environment.apiUrl + 'users/' + this.authService.getUserId() + '/photos',
+      authToken: 'Bearer ' + AuthService.getToken(),
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (!response) {
+        return;
+      }
+      const res: IPhoto = JSON.parse(response);
+      const photo: IPhoto = {
+        id: res.id,
+        url: res.url,
+        dateAdded: res.dateAdded,
+        description: res.description,
+        isMain: res.isMain
+      };
+      this.photos.push(photo);
+    };
+  }
 }
