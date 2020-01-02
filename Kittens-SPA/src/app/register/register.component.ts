235f3109f7ee547d50a84073ac7791224b014a42
oskari.kosonen <o.kosonen@gmail.com>
2020-01-02 10:45:00 +0200

    ReactiveForms + validation for registration

--- Kittens-SPA/src/app/register/register.component.ts
+++ Kittens-SPA/src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { IRegister } from '../api-interfaces';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,14 +13,25 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
 
   public model: IRegister = {};
+  public registerForm: FormGroup;
 
   constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   public ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)])
+    }, this.passwordMatchValidator);
   }
 
+  private passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get("confirmPassword").value
+      ? null
+      : {mismatch: true};
+  }
   public register() {
-    this.authService.register(this.model).subscribe(
+    this.authService.register(this.registerForm.value).subscribe(
       () => { this.alertify.success('success'); },
       (error) => { this.alertify.error(error); }
     );
