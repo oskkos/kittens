0336249a9057fb511c3110477650763a4fbfe16e
oskari.kosonen <o.kosonen@gmail.com>
2020-01-02 09:33:57 +0200

    DeletePhoto API

--- Kittens.API/Controllers/PhotosController.cs
+++ Kittens.API/Controllers/PhotosController.cs
@@ -102,5 +102,38 @@ namespace Kittens.API.Controllers
             }
             return BadRequest("Could not set photo to main");
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePhoto(int userId, int id)
+        {
+   			if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)) {
+				return Unauthorized();
+			}
+			var user = await _repository.GetUser(userId);
+            if (!user.Photos.Any(p => p.Id == id)) {
+                return Unauthorized();
+            }
+            var photoFromRepo = await _repository.GetPhoto(id);
+            if (photoFromRepo.IsMain) {
+                return BadRequest("Can't delete main photo");
+            }
+
+            if (photoFromRepo.PublicId != null)
+            {
+                var deletePars = new DeletionParams(photoFromRepo.PublicId);
+                var result = _cloudinary.Destroy(deletePars);
+
+                if (result.Result == "ok") {
+                    _repository.Delete(photoFromRepo);
+                }
+            }
+            else {
+                _repository.Delete(photoFromRepo);
+            }
+            if (await _repository.SaveAll()) {
+                return Ok();
+            }
+            return BadRequest("Failed to delete the photo");
+        }
 	}
 }
