fd0b092166c17bcc20b2c31e88cae8f77042d003
oskari.kosonen <o.kosonen@gmail.com>
2020-01-04 18:23:36 +0200

    Add pagination component to member list

--- Kittens-SPA/src/app/app.module.ts
+++ Kittens-SPA/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angu
 import { NgModule, Injectable } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BsDropdownModule, TabsModule, BsDatepickerModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule, BsDatepickerModule, PaginationModule } from 'ngx-bootstrap';
 import { CollapseModule } from 'ngx-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
@@ -62,6 +62,7 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       BsDropdownModule.forRoot(),
       BsDatepickerModule.forRoot(),
       TabsModule.forRoot(),
+      PaginationModule.forRoot(),
       CollapseModule.forRoot(),
       RouterModule.forRoot(appRoutes),
       TimeagoModule.forRoot(),
--- Kittens-SPA/src/app/members/member-list/member-list.component.ts
+++ Kittens-SPA/src/app/members/member-list/member-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { IUser } from '../../api-interfaces';
+import { IUser, IPagination, PaginatedResult } from '../../api-interfaces';
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryThumbnailsComponent } from 'ngx-gallery';
 
 @Component({
   selector: 'app-member-list',
@@ -11,12 +12,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberListComponent implements OnInit {
   public users: IUser[];
-
+  public pagination: IPagination;
   constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
   public ngOnInit() {
     this.route.data.subscribe(
-      data => { this.users = data.users.result; }
+      data => {
+        this.users = data.users.result;
+        this.pagination = data.users.pagination;
+      }
     );
+  }
+  public pageChanged(event: {page: number, itemsPerPage: number}) {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+  private loadUsers() {
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+    .subscribe((res: PaginatedResult<IUser[]>) => {
+      this.users = res.result;
+      this.pagination = res.pagination;
+    }, (error) => { this.alertify.error(error); });
   }
 }
