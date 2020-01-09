6d5559e02910e431948bb52dca71f5b04a4631f0
oskari.kosonen <o.kosonen@gmail.com>
2020-01-09 08:09:25 +0200

    Add Likes table

--- Kittens.API/Migrations/20200109060624_AddedLikeEntity.Designer.cs
+++ Kittens.API/Migrations/20200109060624_AddedLikeEntity.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Kittens.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Kittens.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200109060624_AddedLikeEntity")]
+    partial class AddedLikeEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
