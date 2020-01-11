998d588893a0a45bf80ab4337ae9166bd8d93be0
oskari.kosonen <o.kosonen@gmail.com>
2020-01-11 22:15:54 +0200

    Add Message into model

--- Kittens.API/Data/DataContext.cs
+++ Kittens.API/Data/DataContext.cs
@@ -11,12 +11,36 @@ namespace Kittens.API.Data
         public DbSet<User> Users { get; set; }
 		public DbSet<Photo> Photos { get; set; }
 		public DbSet<Like> Likes { get; set; }
+		public DbSet<Message> Messages { get; set; }
 
 		protected override void OnModelCreating(ModelBuilder builder)
 		{
-			builder.Entity<Like>().HasKey(l => new {l.LikerId, l.LikeeId});
-			builder.Entity<Like>().HasOne(l => l.Likee).WithMany(u => u.Likers).HasForeignKey(l => l.LikeeId).OnDelete(DeleteBehavior.Restrict);
-			builder.Entity<Like>().HasOne(l => l.Liker).WithMany(u => u.Likees).HasForeignKey(l => l.LikerId).OnDelete(DeleteBehavior.Restrict);
+			builder.Entity<Like>()
+				.HasKey(l => new {l.LikerId, l.LikeeId});
+
+			builder.Entity<Like>()
+				.HasOne(l => l.Likee)
+				.WithMany(u => u.Likers)
+				.HasForeignKey(l => l.LikeeId)
+				.OnDelete(DeleteBehavior.Restrict);
+
+			builder.Entity<Like>()
+				.HasOne(l => l.Liker)
+				.WithMany(u => u.Likees)
+				.HasForeignKey(l => l.LikerId)
+				.OnDelete(DeleteBehavior.Restrict);
+
+			builder.Entity<Message>()
+				.HasOne(u => u.Sender)
+				.WithMany(m => m.MessagesSent)
+				.HasForeignKey(m => m.SenderId)
+				.OnDelete(DeleteBehavior.Restrict);
+
+			builder.Entity<Message>()
+				.HasOne(u => u.Recipient)
+				.WithMany(m => m.MessagesReceived)
+				.HasForeignKey(m => m.RecipientId)
+				.OnDelete(DeleteBehavior.Restrict);
 		}
 	}
 }
