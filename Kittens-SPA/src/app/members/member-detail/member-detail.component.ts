faeead2db2104684c2c0a52665a3c6a9d0e53422
oskari.kosonen <o.kosonen@gmail.com>
2020-01-12 17:17:47 +0200

    Add navigation directlu to Messages tab within profile

--- Kittens-SPA/src/app/members/member-detail/member-detail.component.ts
+++ Kittens-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {TabsetComponent} from 'ngx-bootstrap';
 import { IUserDetailed } from '../../api-interfaces';
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
@@ -11,6 +12,7 @@ import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gal
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs', {static: true}) public memberTabs: TabsetComponent;
   public user: IUserDetailed;
   public galleryOptions: NgxGalleryOptions[];
   public galleryImages: NgxGalleryImage[];
@@ -21,6 +23,13 @@ export class MemberDetailComponent implements OnInit {
   public ngOnInit() {
     this.route.data.subscribe(
       (data) => { this.user = data.user; }
+    );
+    this.route.queryParams.subscribe(
+      (params) => {
+        if (params.tab) {
+          this.memberTabs.tabs[params.tab].active = true;
+        }
+      }
     );
 
     this.galleryOptions = [
@@ -35,7 +44,9 @@ export class MemberDetailComponent implements OnInit {
     ];
     this.galleryImages = this.getImages();
   }
-
+  public selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
+  }
   private getImages() {
     const imageUrls = [];
     for (const photo of this.user.photos) {
